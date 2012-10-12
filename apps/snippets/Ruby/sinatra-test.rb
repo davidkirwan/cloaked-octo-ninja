920d11957ca3bfd3be6a88ffa5e77f150daed46c
David Kirwan <00346128@mail.wit.ie>
2012-10-13 00:35:29 +0100

    - Updated example after reading: http://ruby.about.com/od/sinatra/a/sinatra2.htm

--- apps/snippets/Ruby/sinatra-test.rb
+++ apps/snippets/Ruby/sinatra-test.rb
@@ -1,5 +1,24 @@
+#!/usr/bin/env ruby
+require 'rubygems'
 require 'sinatra'
 
-get '/hi' do
+get '/' do
   "Hello World!"
 end
+
+
+get '/hello/:name' do
+  @name = params[:name]
+  erb :hello
+end
+
+__END__
+@@ layout
+<html>
+  <body>
+   <%= yield %>
+  </body>
+</html>
+
+@@ hello
+<h3>Hello <%= @name %>!</h3>
