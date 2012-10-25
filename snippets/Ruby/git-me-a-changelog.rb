b81deca7d8afe9e7a037bee4e472cac56bcccac3
David Kirwan <00346128@mail.wit.ie>
2012-10-25 22:51:45 +0100

    Released git-me-a-changelog.rb a Git log scraper to generate CHANGELOG's between one git tag and another

--- snippets/Ruby/git-me-a-changelog.rb
+++ snippets/Ruby/git-me-a-changelog.rb
@@ -7,7 +7,7 @@
 # http:/github.com/davidkirwan
 # Reimagined as a system to generate changelogs between one tag and another.
 #
-# sample usage ./git-changelog-scraper.rb -p ~/Files/Git/Samplerepo v0.0.10 v0.0.6
+# sample usage ./git-me-a-changelog.rb -p ~/Files/Git/Samplerepo v0.0.6 v0.0.10
 #
 ##############################################################################################
 
@@ -22,23 +22,24 @@ class GitMeAChangelog
   def initialize()
     repopath = ARGV[0]
         
-
-    @res = log_to_hash(repopath) || '.'
-    
     unless ARGV[1] == nil then @tag1 = (tag_to_commit_hash(repopath, ARGV[1])).each_line.first; else puts "exiting.."; exit; end
     unless ARGV[2] == nil then @tag2 = (tag_to_commit_hash(repopath, ARGV[2])).each_line.first; else puts "exiting.."; exit; end
-    
+    unless ARGV[0] == nil then @res = log_to_hash(repopath, @tag1, @tag2) || '.'; else puts "exiting.."; exit end
+
     output_summary_report(repopath, ARGV[1], ARGV[2])
 
   end
 
   # scrape the Git log for the target directory then convert to a hash
-  def log_to_hash(repopath)
+  def log_to_hash(repopath, tag1, tag2)
 
     Dir.chdir(repopath)
 
     docs = []
-    cmd = "git log --pretty='%H|%s'"
+    cmd = "git log --pretty='%H|%s' " + tag1 + ".." + tag2
+    cmd = cmd.gsub("\n", "")
+    puts "executing: #{cmd}"
+
     res = `#{cmd}`
     res.each_line do |line|
       parts = line.split('|')
@@ -53,15 +54,18 @@ class GitMeAChangelog
     puts "Tag Commit Hashes:\n#{tag1}: #{@tag1}#{tag2}: #{@tag2}\n\n"
     
     @res.each do |i|
-      puts i[:commit] + " " + i[:subject]
+      puts i[:subject]
     end
   end
 
 
   def tag_to_commit_hash(repopath, tag)
-
+    Dir.chdir(repopath)
     argline = "git log #{tag} --pretty='%H' "
+    puts "executing: #{argline}"
+
     output=`#{argline} 2>&1` 
+
     #if result=$?.success?
       return output
     #else
@@ -74,4 +78,5 @@ end
 
 
 # Launch the system
+puts "Usage: ./gitMeAChangelog.rb <path/to/git/repo> <startingtag> <endingtag>\n\n"
 g = GitMeAChangelog.new()
