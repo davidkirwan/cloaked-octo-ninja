446f53e60edc6b418adb9524f91efd4d55f62f9f
David Kirwan <00346128@mail.wit.ie>
2013-06-09 14:45:05 +0100

    - Updated Ruby snippet for JSON

--- snippets/Ruby/json.rb
+++ snippets/Ruby/json.rb
@@ -2,13 +2,16 @@
 # @author 	David Kirwan https://github.com/davidkirwan
 # @description 	Sample script working with the JSON class in Ruby
 #
-# @date 	22-10-2012
+# @date 	09-06-2013
 ####################################################################################################
 require 'rubygems'
 require 'json'
 
+# Create a test data hash
 data = {"sequence" => 5, "data" => [23.5, 997.5, 65], "key" => "1234567890abcdef" }
 
+# call the to_json method on this hash to convert the hash to a JSON string
 puts data.to_json
 
+# indent the JSON string in a human readable styled string
 puts JSON.pretty_generate(JSON.parse(data.to_json))
