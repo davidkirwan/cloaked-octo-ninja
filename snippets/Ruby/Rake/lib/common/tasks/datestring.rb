29d00bce807618b118982f58452ff9a0b20b0076
David Kirwan <davidkirwanirl@gmail.com>
2013-07-13 02:54:46 +0100

    - Added Common::Tasks::DateString which returns a String YYYY-MM-DD - Implemeted skeleton tasks for generating an archive from a path

---
+++ snippets/Ruby/Rake/lib/common/tasks/datestring.rb
@@ -0,0 +1,35 @@
+####################################################################################################
+# @author       David Kirwan https://github.com/davidkirwan
+# @description  DateString Class
+#
+# @date         2013-07-12
+####################################################################################################
+# Imports
+require 'logger'
+
+module Common
+  module Tasks
+
+
+  	##
+    # Common::Tasks::DateString#now Returns a String with todays date in the form YYYY-MM-DD
+    #
+    # * *Args* :
+    # - ++ ->   
+    # * *Returns* :
+    # -         String
+    # * *Raises* :
+    #           
+    #
+  	class DateString
+
+  	  def self.now
+  	  	now = DateTime.now
+  		return now.year.to_s + "-" + "%02d" % now.month + "-" + "%02d" % now.day
+  	  end
+
+  	end # End of the DateString class
+
+
+  end
+end
