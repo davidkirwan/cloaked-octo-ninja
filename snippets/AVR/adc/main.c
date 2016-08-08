b698bd81caa82f6eeeab99ebb73dddd5cc700a24
David Kirwan <davidkirwanirl@gmail.com>
2016-08-08 02:12:32 +0100

    - ADC example, led on PB0, ADC on PB4 or ADC2

---
+++ snippets/AVR/adc/main.c
@@ -0,0 +1,82 @@
+/**
+ **/
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED_PORT PB0
+
+
+void initADC()
+{
+  /* this function initialises the ADC 
+
+        ADC Prescaler Notes:
+	--------------------
+
+	   ADC Prescaler needs to be set so that the ADC input frequency is between 50 - 200kHz.
+  
+           For more information, see table 17.5 "ADC Prescaler Selections" in 
+           chapter 17.13.2 "ADCSRA – ADC Control and Status Register A"
+          (pages 140 and 141 on the complete ATtiny25/45/85 datasheet, Rev. 2586M–AVR–07/10)
+
+           Valid prescaler values for various clock speeds
+	
+	     Clock   Available prescaler values
+           ---------------------------------------
+             1 MHz   8 (125kHz), 16 (62.5kHz)
+             4 MHz   32 (125kHz), 64 (62.5kHz)
+             8 MHz   64 (125kHz), 128 (62.5kHz)
+            16 MHz   128 (125kHz)
+
+           Below example set prescaler to 128 for mcu running at 8MHz
+           (check the datasheet for the proper bit values to set the prescaler)
+  */
+
+  // 8-bit resolution
+  // set ADLAR to 1 to enable the Left-shift result (only bits ADC9..ADC2 are available)
+  // then, only reading ADCH is sufficient for 8-bit results (256 values)
+
+  ADMUX =
+            (1 << ADLAR) |     // left shift result
+            (0 << REFS1) |     // Sets ref. voltage to VCC, bit 1
+            (0 << REFS0) |     // Sets ref. voltage to VCC, bit 0
+            (0 << MUX3)  |     // use ADC2 for input (PB4), MUX bit 3
+            (0 << MUX2)  |     // use ADC2 for input (PB4), MUX bit 2
+            (1 << MUX1)  |     // use ADC2 for input (PB4), MUX bit 1
+            (0 << MUX0);       // use ADC2 for input (PB4), MUX bit 0
+
+  ADCSRA = 
+            (1 << ADEN)  |     // Enable ADC 
+            (1 << ADPS2) |     // set prescaler to 64, bit 2 
+            (1 << ADPS1) |     // set prescaler to 64, bit 1 
+            (0 << ADPS0);      // set prescaler to 64, bit 0  
+}
+
+
+int main(void) 
+{
+  initADC();
+  DDRB |= (1 << LED_PORT); // Sets PB0 as an output
+
+  while(1)
+  {
+    ADCSRA |= (1 << ADSC);         // start ADC measurement
+    while (ADCSRA & (1 << ADSC) ); // wait till conversion complete 
+
+    if (ADCH > 128)
+    {
+      // ADC input voltage is more than half of VCC
+      // Set the LED bit to "1" - LED will be "on".
+      PORTB |= (1 << LED_PORT);
+
+    } else {
+      // ADC input voltage is less than half of VCC
+      // Set the LED bit to "0" - LED will be "off".
+      PORTB &= ~(1 << LED_PORT);
+    }
+  }
+
+  return 0;
+}
+
