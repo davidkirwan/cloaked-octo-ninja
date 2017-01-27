e7360b848f635e2ee4b63f7439a33716fe703898
David Kirwan <dkirwan@redhat.com>
2017-01-27 23:04:21 +0000

    Moving to its own repository

--- config/vagrant-container/scripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-echo "Stopping all containers"
-docker stop $(docker ps -a -q)
-
-echo "Removing all containers"
-docker rm $(docker ps -a -q)
-
-echo "Removing dangling images"
-docker rmi $(docker images -f dangling=true -q)
--- config/vagrant-container/scripts/populate_variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-IP=$1
-PORT=$2
-
-echo $1:$2
-sed "s/{{ IP }}/${IP}/g" /srv/vagrant/docker/nginx/templates/default.template > /tmp/default.conf_1
-sed "s/{{ PORT }}/${PORT}/g" /tmp/default.conf_1 > /srv/vagrant/docker/nginx/default.conf
